22070f176f5aa3e46e50055a1a3209d25e886559
torvista <torvista@users.noreply.github.com>
2019-03-27 12:10:41 +0100

    new files

---
+++ files_usted/ADMIN_NAME/includes/languages/spanish/gv_name.php
@@ -0,0 +1,4 @@
+<?php //Spanish Language Pack for Zen Cart 1.5x: https://github.com/torvista/Zen-Cart-1.5x-Spanish-Language-Pack
+// Define the name of your Gift Certificate as Gift Voucher, Gift Certificate, Zen Cart Dollars, etc. here for use through out the shop
+  define('TEXT_GV_NAME','Tarjeta Regalo');
+  define('TEXT_GV_NAMES','Tarjetas de Regalo');
